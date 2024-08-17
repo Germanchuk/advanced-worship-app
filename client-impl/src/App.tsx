96dd57871870906581f22abdf7757e2db7668bf1
Volodymyr Herman <vova.german1789@gmail.com>
2024-08-17 22:31:28 +0300

    initialize client and backend impl

---
+++ client-impl/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Block from './components/Block/Block';
+import TextArea from './components/TextArea/TextArea';
+
+function App() {
+  return (
+    <div className="App">
+      <Block />
+      <TextArea />
+    </div>
+  );
+}
+
+export default App;
